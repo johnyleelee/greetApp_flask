d75deefc318adae85ce2212f76bb776f5b450479
johnny leelee <131977391+johnyleelee@users.noreply.github.com>
2023-04-29 21:19:17 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask,render_template,request,flash
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-app.secret_key = "Abandon89_flashing"
+app.secret_key = "mbandonuisss"
 
 @app.route("/")
 def index():
-    flash("What's your name? ")
-    return render_template("index.html")
+	flash("what's your name?")
+	return render_template("index.html")
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
 	return render_template("index.html")
 
+#if __name__ == "__main__":
+#    app.run(debug=True)
