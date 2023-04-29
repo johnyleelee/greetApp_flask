aa3375a0753cb7f4a89473f81eb8ccb3db62a2f4
johnny leelee <131977391+johnyleelee@users.noreply.github.com>
2023-04-29 22:51:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -12,6 +12,3 @@ def index():
 def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
 	return render_template("index.html")
-
-#if __name__ == "__main__":
-#	app.run(debug=True)
